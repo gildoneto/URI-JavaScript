50a8adb3b7c108a96c2ec16c260deb6dc99efb55
Gildo Neto <gildoneto@hotmail.com>
2021-10-21 22:14:06 -0300

    Meu primeiro commit

---
+++ Iniciante/1000/1000.js
@@ -0,0 +1,8 @@
+// barra barra = comentario (ignorado pelo compilador)
+// imprime tudo que estiver declarado
+// no parametro, no console.
+let saudacao = 'Hello World!'
+saudacao = 'Ola Mundo' sdfsdf
+saudacao = 34
+console.log(saudacao)
+console.log('Hello World!')
---
+++ Iniciante/1001/1001.js
@@ -0,0 +1,10 @@
+// A funcao prompt recebe um valor como parametro
+// que sera exibido na caixa de pergunta
+// A funcao retorna um valor do tipo string
+// o valor que foi digitado
+let num1 = parseInt(prompt('Digita o primeiro numero aew bença'));
+let num2 = parseInt(prompt('Digita o segundo numero aew bença'));
+
+let resultado = num1 + num2;
+
+console.log('RESULTADO = '+ resultado);
