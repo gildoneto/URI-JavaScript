dc7bb0c70f1f4ea34ed28a38f23c53e658ff865d
Gildo Neto <gildoneto@hotmail.com>
2021-10-28 21:33:26 -0300

    subindo agora o 1004.js

---
+++ Iniciante/1004/1004.js
@@ -0,0 +1,19 @@
+// Leia dois valores inteiros.
+// calcule o produto entre estes dois valores 
+// e atribua esta operação à variável PROD.
+// A seguir mostre a variável PROD com mensagem 
+//correspondente.
+// exemplo: 10 => PROD = 10
+// DEIXE SEMPRE UMA LINHA EM BRANCO NO FINAL DO SEU CODIGO
+
+// MEMORIA RAM
+// num1: undefined
+// num2: undefined
+// PROD: undefined
+
+let num1 = parseInt(prompt('Digite um numero'));
+let num2 = parseInt(prompt('Digite outro numero'));
+
+let PROD = (num1 * num2)
+
+console.log('PROD = ' + PROD)
