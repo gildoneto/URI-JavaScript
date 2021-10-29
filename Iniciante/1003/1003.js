f58f022fcfb581ea58b44d23f842a52288f345da
Gildo Neto <gildoneto@hotmail.com>
2021-10-28 21:03:57 -0300

    entrega exercicio 1003

---
+++ Iniciante/1003/1003.js
@@ -0,0 +1,12 @@
+// Leia dois valores inteiros, no caso para variáveis A e B. Check
+// A seguir, calcule a soma entre elas e atribua à variável SOMA.
+// RETORNAR - ATRIBUIR - OPERAÇÃO aritimetica - CONCATENAR(JUNTAR)
+// CHAMADA DE FUNCAO
+// a funcao prompt retorna o valor digitado do tipo string
+
+let A = parseInt(prompt('Digite um valor inteiro'));
+let B = parseInt(prompt('Digite OUTRO valor inteiro'));
+
+let SOMA = A + B;
+
+console.log(SOMA)
